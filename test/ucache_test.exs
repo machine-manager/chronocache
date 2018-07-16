05aca52c73ea9425be961109c026ceb14c72a3a0
Ivan Kozik <ivan@ludios.org>
2018-07-16 06:11:25 +0000

    WIP

--- test/ucache_test.exs
+++ test/ucache_test.exs
@@ -10,22 +10,6 @@ defmodule UCacheTest do
     assert 10 == UCache.get_or_run(table, :key, fn -> 10 end)
   end
 
-  defp cache_single(table, wait_time, result) do
-    :ets.insert(table, {:fun_lock, false})
-    :ets.insert(table, {:call_count, 0})
-
-    case :ets.lookup(table, :fun_lock) do
-      [{:fun_lock, false}] ->
-        :ets.insert(table, {:fun_lock, true})
-        Process.sleep(wait_time)
-        :ets.insert(table, {:fun_lock, false})
-        result
-
-      _ ->
-        raise "not single"
-    end
-  end
-
   test "many processes call get_or_run, but one process only calls the caching function" do
     table = UCache.new()
 
@@ -48,19 +32,19 @@ defmodule UCacheTest do
     end
   end
 
-  defp cache_raise(wait_time, result) do
-    table = UCache.new()
+  defp cache_single(table, wait_time, result) do
+    :ets.insert(table, {:fun_lock, false})
+    :ets.insert(table, {:call_count, 0})
 
-    case :ets.update_counter(table, :call_count, {2, 1}) do
-      1 ->
-        # first call is failed
+    case :ets.lookup(table, :fun_lock) do
+      [{:fun_lock, false}] ->
+        :ets.insert(table, {:fun_lock, true})
         Process.sleep(wait_time)
-        raise "failed"
+        :ets.insert(table, {:fun_lock, false})
+        result
 
       _ ->
-        # other calls are passed
-        Process.sleep(wait_time)
-        result
+        raise "not single"
     end
   end
 
@@ -69,12 +53,12 @@ defmodule UCacheTest do
 
     fun1 = fn ->
       assert_raise(RuntimeError, "failed", fn ->
-        UCache.get_or_run(table, :key, fn -> cache_raise(100, 20) end)
+        UCache.get_or_run(table, :key, fn -> cache_raise(table, 100, 20) end)
       end)
     end
 
     fun2 = fn ->
-      assert 30 == UCache.get_or_run(table, :key, fn -> cache_raise(100, 30) end)
+      assert 30 == UCache.get_or_run(table, :key, fn -> cache_raise(table, 100, 30) end)
     end
 
     ps =
@@ -96,6 +80,20 @@ defmodule UCacheTest do
           assert ps[pid] == ref
           assert reason == :normal
       end
+    end
+  end
+
+  defp cache_raise(table, wait_time, result) do
+    case :ets.update_counter(table, :call_count, {2, 1}) do
+      1 ->
+        # first call is failed
+        Process.sleep(wait_time)
+        raise "failed"
+
+      _ ->
+        # other calls are passed
+        Process.sleep(wait_time)
+        result
     end
   end
 end
